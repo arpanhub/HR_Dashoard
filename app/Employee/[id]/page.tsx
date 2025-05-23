67a32faa4e971bb961c21e9f89e8ffe4f1b9e6e5
Arpan Gupta <arpan.gupta@lpu.in>
2025-05-23 20:59:54 +0530

    fixed the params error

--- app/Employee/[id]/page.tsx
+++ app/Employee/[id]/page.tsx
@@ -1,4 +1,9 @@
-export default function EmployeePage({ params }: any) {
+interface pageProps{
+  params:{
+    id:string;
+  };
+}
+export default async function EmployeePage({ params }: pageProps) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold">Employee Details</h1>
