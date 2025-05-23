714046ec91335714fab600b037a65bb9462e2ad4
Arpan Gupta <arpan.gupta@lpu.in>
2025-05-23 20:30:51 +0530

    fixed type linting error

--- app/Employee/[id]/page.tsx
+++ app/Employee/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default function EmployeePage({ params }: { params: { id: string } }) {
+export default function EmployeePage({ params }: any) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-2xl font-bold">Employee Details</h1>
